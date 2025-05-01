59c7870623665e25b93f2958279b7de3d3103e5b
b1329037 <sophiechang0731@gmail.com>
2025-05-01 20:48:58 +0800

    Update main25.c

--- main25.c
+++ main25.c
@@ -1,20 +1,35 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 
-int multi(int i, int j) {
-    if (j == 1) {
-        printf("%d*%d=%2d ", i, j, i*j);
-        return 0;
-    } else {
-        multi(i, j - 1);
-        printf("%d*%d=%2d ", i, j, i*j);
-        return 0;
-    }
-}
+using namespace std;
 
-int main() {
-    for (int i = 1; i <= 9; i++) {
-        multi(i, 9);  // 呼叫原本的遞迴函數印出每一列
-        printf("\n"); // 每列之後換行
+class Car {
+    public:
+    string m_brand;
+    string m_model;
+    int m_year;
+    int m_MaxSeating;
+    int m_price;
+    Car(string x, string y, int z, int s){
+        m_brand =x;
+        m_model =y;
+        m_year =z;
+        m_MaxSeating =s;
+        m_price =s*500000;
     }
+    int get_MaxSeating(){
+        return m_MaxSeating;
+    }
+    int get_price(){
+        return m_price;
+    }
+};
+
+int main()
+{
+    Car car_1("BMW","X1",2023,6);
+    Car car_2("Audi","A1",2021,2);
+    cout << car_1.m_brand << " Max Seating = "<<car_1.get_MaxSeating()<<endl<<"price = "<< car_1.get_price()<< endl;
+    cout << car_2.m_brand << " Max Seating = "<<car_2.get_MaxSeating()<<endl<<"price = "<< car_2.get_price()<< endl;
     return 0;
 }
