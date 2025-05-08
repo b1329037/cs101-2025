e4ad5ffd65abfbc2dc5344e67e4d2fbd809b031b
b1329037 <sophiechang0731@gmail.com>
2025-05-08 14:29:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,84 +1,48 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class Car{
-    protected:
-    string m_DriveMode;
-    private:
-    int m_MaxSeating;
-    int m_price;
-    void m_UpdatePrice(int base =500000){
-        m_price = m_MaxSeating*base;
+class Car {
+public:
+    string brand;
+    string driveMode;
+
+    Car(string b, string d) {
+        brand = b;
+        driveMode = d;
+        cout << "Constructing " << brand << "_Car" << endl;
     }
-    public:
-    string m_brand;
-    string m_model;
-    int m_year;
-    
-    Car(string x, string y, int z, int s){
-        m_brand =x;
-        m_model =y;
-        m_year =z;
-        m_MaxSeating =s;
-        m_UpdatePrice();
-        m_DriveMode = "No-wheel";
-    }
-    int get_MaxSeating(){
-        return m_MaxSeating;
-    }
-    int get_price(){
-        return m_price;
-    }
-    string get_DriveMode(){
-        return m_DriveMode;
+
+    void showDriveMode() {
+        cout << brand << " : Drive Mode = " << driveMode << endl;
     }
 };
 
-class BMW_Car : public Car{
-    public:
-    BMW_Car(string y,int z,int s): Car("BMW",y,z,s){
-        cout << "Constructing BMW_Car\n";
-        m_DriveMode ="Rear-wheel";
-    }
-    string get_DriveMode(){
-        return m_DriveMode;
-    }
+class BMW_Car : public Car {
+public:
+    BMW_Car() : Car("BMW", "Rear-wheel") {}
 };
-class Audi_Car : public Car{
-    public:
-    Audi_Car(string y,int z,int s): Car("Audi",y,z,s){
-        cout << "Constructing Audi_Car\n";
-        m_DriveMode ="Front-wheel";
-    }
-    string get_DriveMode(){
-        return m_DriveMode;
-    }
+
+class AUDI_Car : public Car {
+public:
+    AUDI_Car() : Car("Audi", "Front-wheel") {}
 };
-class BENZ_Car : public Car{
-    public:
-    BENZ_Car(string y,int z,int s): Car("BENZ",y,z,s){
-        cout << "Constructing BENZ_Car\n";
-        m_DriveMode ="Front-wheel";
-    }
-    string get_DriveMode(){
-        return m_DriveMode;
-    }
+
+class BENZ_Car : public Car {
+public:
+    BENZ_Car() : Car("Benz", "Front-wheel") {}
 };
-int main()
-{
-    BMW_Car car_1("X5",2023,6);
-    cout << car_1.m_brand;
-    cout << " : DriveMode = " <<car_1.get_DriveMode()<<endl;
-    
-    Audi_Car car_2("A1",2023,5);
-    cout << car_2.m_brand;
-    cout << " : DriveMode = " <<car_2.get_DriveMode()<<endl;
-    
-    BENZ_Car car_3("class",2023,4);
-    cout << car_3.m_brand;
-    cout << " : DriveMode = " <<car_3.get_DriveMode()<<endl;
+
+
+int main() {
+    BMW_Car bmw;
+    bmw.showDriveMode();
+
+    AUDI_Car audi;
+    audi.showDriveMode();
+
+    BENZ_Car benz;
+    benz.showDriveMode();
 
     return 0;
 }
